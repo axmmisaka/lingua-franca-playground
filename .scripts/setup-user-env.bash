f4234213f62d1edaf8f41428349ce064430eefff
Izumi Sagiri <axmmisaka@users.noreply.github.com>
2023-07-10 23:14:25 -0700

    Major refactoring
    1. Split setup.bash into different files to allow users to run them separately and allow parallelism
    2. Do not install nvm and sdkman during dockerfile creation if one does not exist in the base image
    3. Added setup-env.bash to allow users install nvm and sdkman for user only
    4. Purged noexample
    5. Changed configs accoringly

---
+++ .scripts/setup-user-env.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+# This script specifically detects and set up nvm and SDKMAN in bash environment, and install
+# needed components for LF
+# TODO: remove some nvm checks and simply use interactive shell
+set -euxo pipefail
+
+# Check if SDK is installed like what SDKMAN installer does
+if [ -z "${SDKMAN_DIR:-}" ] ; then
+    echo "SDKMAN not found. Installing"
+    curl -s "https://get.sdkman.io" | bash
+    export SDKMAN_DIR="$HOME/.sdkman"
+fi
+
+set +ux
+\. "$SDKMAN_DIR/bin/sdkman-init.sh"
+sdk install java 17.0.7-ms <<< "y"
+sdk use java 17.0.7-ms
+set -ux
+
+
+# Check if nvm is installed
+NVM_INSTALLED=false
+if [ -d "${HOME}/.nvm/.git" ]; then 
+    NVM_INSTALLED=true;
+    export NVM_DIR="${HOME}/.nvm"
+fi
+# Check if nvm is in .bashrc
+NVM_BASHRC_COMMAND=$(grep -e "export NVM_DIR" "$HOME/.bashrc")
+if [ -n "${NVM_BASHRC_COMMAND}" ]; then
+    NVM_INSTALLED=true
+    set +ux
+    eval "${NVM_BASHRC_COMMAND}"
+    set -ux
+fi
+if [ "$NVM_INSTALLED" = false ]; then
+    set +ux
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    set -ux
+    export NVM_DIR="${HOME}/.nvm"
+
+set +ux
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install --lts
+nvm use --lts
+npm install --global typescript pnpm
+
--- dockerfiles/lf-base.Dockerfile
+++ dockerfiles/lf-base.Dockerfile
@@ -1,6 +1,8 @@
 FROM ubuntu:focal
 
 COPY .scripts .scripts
-RUN bash ./.scripts/setup.bash --dependencies-only &&\
-    bash -c "curl -s "https://get.sdkman.io" | bash && source /home/gitpod/.sdkman/bin/sdkman-init.sh && sdk install java 19.0.2-open < /dev/null"
+RUN bash ./.scripts/setup-env.bash 
+    # Uncomment this line if you want SDKMAN to be installed with Docker.
+    # A better way is to run setup-user-env
+    # bash -c "curl -s "https://get.sdkman.io" | bash && source /home/gitpod/.sdkman/bin/sdkman-init.sh && sdk install java 19.0.2-open < /dev/null"
 # To be continued......
